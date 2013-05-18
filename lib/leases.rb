225fbe738c27838cbeb593ca6e910edaa141546b
Arjen Oosterkamp <mail@arjen.me>
2013-05-18 21:12:42 +0200

    Configure Apartment to use database_names proc
    Also preloads Rails app because it needs to know about the leaser models

--- lib/leases.rb
+++ lib/leases.rb
@@ -5,31 +5,34 @@ require 'leases/controller'
 
 module Leases
 
-  class << self
+  extend self
 
-    cattr_accessor :leasers
-    self.leasers = []
+  attr_accessor :leasers
+  self.leasers = []
 
-    def leasing(object)
-      self.leasers << object.name
-      self.leasers.uniq!
+  # Add leaser to array of leasers.
+  #
+  # => leasing(Account)
+  def leasing(object)
+    self.leasers << object.name
+    self.leasers.uniq!
 
-      Apartment.excluded_models ||= []
-      Apartment.excluded_models += [object.name]
-      Apartment.excluded_models.uniq!
+    Apartment.excluded_models ||= []
+    Apartment.excluded_models += [object.name]
+    Apartment.excluded_models.uniq!
 
-      Apartment.database_names ||= Proc.new do
-        Leases.leaser_names
-      end
-    end
+    Apartment.database_names = Proc.new { Leases.leaser_names }
 
-    def leaser_names
-      leasers.map do |l|
-        model = l.constantize
-        model.all.collect(&:leaser_name)
-      end.flatten
-    end
+    object
+  end
 
+  def leaser_names(preload=true)
+    Rails.application.eager_load! if preload
+
+    leasers.map do |l|
+      model = l.constantize
+      model.all.collect(&:leaser_name)
+    end.flatten
   end
 
 end
--- lib/leases/railtie.rb
+++ lib/leases/railtie.rb
@@ -3,5 +3,10 @@ require 'apartment'
 
 module Leases
   class Railtie < Rails::Railtie
+
+    config.to_prepare do
+      Apartment.database_names = Proc.new { Leases.leaser_names }
+    end
+
   end
 end
